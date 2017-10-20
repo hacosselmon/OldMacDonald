e4a4a1f6037443f1b03874171ae4b9c9fd44f566
hacosselmon <hacosselmon@s.sfusd.edu>
2017-10-20 13:40:48 -0700

    new

--- Chick.java
+++ Chick.java
@@ -1,4 +1,20 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal
+{
+  private String myType;
+  private String mySound;
+  public Chick(String type, String sound1, String sound2)
+  {
+     myType = type;
+     if (((double)(Math.random()*1))>0.5)
+       mySound = sound1;
+     else
+       mySound = sound2;
+  }
+  public Chick()
+  {
+    myType = "unknown";
+    mySound = "unknown";
+  }
+  public String getSound(){return mySound;}
+  public String getType(){return myType;}
 }
